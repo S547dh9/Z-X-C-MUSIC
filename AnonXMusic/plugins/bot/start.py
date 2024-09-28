216e98c76642342f24dbaeb0d80ad2fc515415b0
S547dh9 <84910430+S547dh9@users.noreply.github.com>
2024-09-28 11:26:09 +0530

    Update start.py

--- AnonXMusic/plugins/bot/start.py
+++ AnonXMusic/plugins/bot/start.py
@@ -88,7 +88,17 @@ async def start_pm(client, message: Message, _):
             photo=config.START_IMG_URL,
             caption=_["start_2"].format(message.from_user.mention, app.mention),
             reply_markup=InlineKeyboardMarkup(out), 
-        )
+      
+        )  
+        
+             # Ab start_3 caption ko reply_markup ke saath bhejo
+        await message.reply_text(
+            text=_["start_3"].format(app.mention),  # Dusra caption (start_3)
+            reply_markup=InlineKeyboardMarkup(out),  # reply_markup ke saath
+        
+        
+        )  
+        
         if await is_on_off(2):
             return await app.send_message(
                 chat_id=config.LOGGER_ID,
